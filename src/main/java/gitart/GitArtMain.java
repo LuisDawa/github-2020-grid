e0893bf3aeb05423bd04b56b3407ec72e65d2c41
Luís Dawa <luisdawa@gmail.com>
2025-08-11 22:03:55 -0300

    lol

--- src/main/java/gitart/GitArtMain.java
+++
@@ -1,9 +0,0 @@
-package gitart;
-
-import javax.swing.SwingUtilities;
-
-public class GitArtMain {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new GitArtFrame());
-    }
-}
